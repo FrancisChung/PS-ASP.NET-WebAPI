fd80a2fcdd698d103e28406254c6567a13dc9c05
Francis Chung <francis.c.chung@gmail.com>
2020-02-09 08:43:55 +1100

    Location Fields added

--- Starting/TheCodeCamp/Models/CampModel.cs
+++ Starting/TheCodeCamp/Models/CampModel.cs
@@ -9,5 +9,15 @@ namespace TheCodeCamp.Models
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+
+        //Location Info (copied from Location Entity)
+        public string LocationVenueName { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
     }
 }
