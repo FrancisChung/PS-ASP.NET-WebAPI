836df6c96e009745c207d5d9b80371f45c440d93
Francis Chung <francis.c.chung@gmail.com>
2020-02-08 21:32:58 +1100

    Changed ContractResolver to CamelCase

--- Starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ Starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace TheCodeCamp
 {
@@ -11,6 +12,9 @@ namespace TheCodeCamp
     {
       // Web API configuration and services
       AutofacConfig.Register();
+
+      //Change the casing of JsonFormatting
+      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
       // Web API routes
       config.MapHttpAttributeRoutes();
