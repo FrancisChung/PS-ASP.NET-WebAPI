607e936e9f89986371e1f92d5df41211b99916b6
Francis Chung <francis.c.chung@gmail.com>
2020-02-08 18:15:29 +1100

    CampController to use CampRepository

--- Starting/TheCodeCamp/Controllers/CampsController.cs
+++ Starting/TheCodeCamp/Controllers/CampsController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using TheCodeCamp.Data;
 
 namespace TheCodeCamp.Controllers
 {
     public class CampsController :ApiController
     {
-        public IHttpActionResult Get()
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
         {
-            //return BadRequest("Not really bad. Just testing");
-            return Ok(new {Name = "Francis", Occupation = "Developer"});
+            _repository = repository;
+        }
+
+         
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch
+            {
+                // Don't return all exceptions that happens in Server for security reasons
+                return InternalServerError();
+            }
         }
     }
 }
