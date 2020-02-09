9584f5e3ff61bdca3ad4c377e2b428ef4b089f8a
FrancisChung <francis.c.chung@gmail.com>
2020-02-10 08:18:49 +1100

    PUT Implementation

--- Starting/TheCodeCamp/Controllers/CampsController.cs
+++ Starting/TheCodeCamp/Controllers/CampsController.cs
@@ -107,5 +107,27 @@ namespace TheCodeCamp.Controllers
 
             return BadRequest(ModelState);
         }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+                if (camp == null)
+                    return NotFound();
+
+                _mapper.Map(model, camp);
+
+                if (await _repository.SaveChangesAsync())
+                    return Ok(_mapper.Map<CampModel>(camp));
+                else
+                    return InternalServerError();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
     }
 }
